b244cb1cc88d03cac29520c5501c4fcba9138827
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-08-26 20:38:17 +0700

    nopbai

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace MinArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input array size: ");
+            int size = Int32.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            Console.WriteLine("Input your array: ");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = Int32.Parse(Console.ReadLine());
+            }
+            int index = minValue(array);
+            Console.WriteLine("MIN is: "+ array[index]);
+
+        }
+        public static int minValue(int[] array)
+        {
+            int min = array[0];
+            int index = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+
+    }
+}
+
